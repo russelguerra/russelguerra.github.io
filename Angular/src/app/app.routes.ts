943204f7327a18a5e70a98db3f67f59da749f899
Russel <russelguerra@gmail.com>
2025-05-03 03:03:54 +0800

    New profile

---
+++ Angular/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ResumeComponent } from './pages/resume/resume.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: ResumeComponent,
+    }
+];
--- pages/index/indexController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-    var indexApp = angular.module("indexApp");
-
-    indexApp.controller("indexController", function($scope){
-        $scope.test = "asd";
-    });
-})();
